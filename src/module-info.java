e90e7cf86e8bc5c281110bc81e6f30f7d8d2d688
Connor <connordecamp@gmail.com>
2020-07-15 08:09:55 -0500

    Add files via upload

---
+++ src/module-info.java
@@ -0,0 +1,12 @@
+module calculator 
+{
+	requires javafx.base;
+	requires javafx.controls;
+	requires javafx.fxml;
+	requires javafx.graphics;
+	requires transitive javafx.media;
+	requires javafx.swing;
+	requires javafx.web;
+	
+	exports calculator;
+}
